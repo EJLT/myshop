19041fe0e214ce5c32ee517d93b7db9bd3e3d4a1
EJLT <81562702+EJLT@users.noreply.github.com>
2021-09-03 16:58:51 +0200

    Averías

--- src/main/java/com/sanvalero/myshop/domain/Issues.java
+++ src/main/java/com/sanvalero/myshop/domain/Issues.java
@@ -1,2 +1,15 @@
-package com.sanvalero.myshop.domain;public class Issues {
+package com.sanvalero.myshop.domain;
+
+public class Issues {
+
+
+
+    private int id;
+    private String description;
+    private String problem;
 }
+
+
+
+
+
