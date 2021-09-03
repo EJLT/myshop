699eb4298f1789dcbdaa2c64c786c6b653358464
EJLT <81562702+EJLT@users.noreply.github.com>
2021-09-03 16:52:09 +0200

    Actualización

--- src/main/java/com/sanvalero/myshop/domain/User.java
+++ src/main/java/com/sanvalero/myshop/domain/User.java
@@ -2,11 +2,17 @@ package com.sanvalero.myshop.domain;
 
 import com.sun.org.apache.xpath.internal.objects.XString;
 
-public class User {
+public abstract class User {
 
     private long id;
     private String username;
     private String password;
+    private String name;
+    private String surname;
+    private String email;
+    private String birthday;
+    private String city;
+
 
 
     }
